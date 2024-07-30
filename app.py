bab9f305d13af23e201b8c55f5b11bf65faf2bef
Borja Guanche <bg.sicilia@gmail.com>
2024-07-30 14:42:56 +0100

    Update APP

---
+++ app.py
@@ -0,0 +1,22 @@
+from dash import Dash, dcc, html, Input, Output, callback
+
+
+app = Dash(__name__)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
